9c41931853edba81c14ce3b9afa5d89f834ea6c4
Aaron Resch <aaronmresch@gmail.com>
2021-03-16 13:56:29 -0500

    added classes for heist crew, implemented IRobber

---
+++ LockSpecialist.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace HeistPartDeux
+{
+    public class LockSpecialist : IRobber
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public int PercentageCut { get; set; }
+
+        public void PerformSkill(Bank bank)
+        {
+            bank.VaultScore -= SkillLevel;
+            Console.WriteLine($"{Name} is cracking the vault. Decreased security {SkillLevel} points!");
+            if (bank.VaultScore <= 0)
+            {
+                Console.WriteLine($"{Name} broke into the vault!");
+            }
+        }
+    }
+}
