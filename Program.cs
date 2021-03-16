a5ee224a8636ae23ec27971126f18c644d6f53a8
Aaron Resch <aaronmresch@gmail.com>
2021-03-16 11:28:39 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeistPartDeux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
